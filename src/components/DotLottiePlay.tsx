0bc7f8c462b92ca4e42f644fe1030caa1c8a8681
fahanmante <fa.hanmante@shiftboolean.com>
2025-09-09 14:00:05 +0530

    using lottie from lottie host

--- src/components/DotLottiePlay.tsx
+++ src/components/DotLottiePlay.tsx
@@ -3,14 +3,30 @@ import { DotLottiePlayer } from "@dotlottie/react-player";
 
 const DotLottiePlayerAnimation = () => {
   return (
-    <DotLottiePlayer
-      src="/lotties/sample.lottie"
-      style={{
-        height: 350,
-        width: 350,
-      }}
-      loop
-    />
+    <div className="flex flex-row gap-5">
+      {/* taking path from public folder or locally served */}
+      <DotLottiePlayer
+        src="/lotties/sample.lottie"
+        style={{
+          height: 350,
+          width: 350,
+        }}
+        loop
+        autoplay
+      />
+      {/* taking path from external url */}
+      <DotLottiePlayer
+        src={
+          "https://lottie.host/138051fd-70a3-40f3-8939-b5a7ae8c6a04/B0xz2h3pRf.lottie"
+        }
+        style={{
+          height: 350,
+          width: 350,
+        }}
+        loop
+        autoplay
+      />
+    </div>
   );
 };
 

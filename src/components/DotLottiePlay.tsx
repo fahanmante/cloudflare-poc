acb3ce10886899bd9fe54355c268ef66d33c5f93
fahanmante <fa.hanmante@shiftboolean.com>
2025-09-09 11:22:30 +0530

    serving lottie from public folder

--- src/components/DotLottiePlay.tsx
+++ src/components/DotLottiePlay.tsx
@@ -4,9 +4,7 @@ import { DotLottiePlayer } from "@dotlottie/react-player";
 const DotLottiePlayerAnimation = () => {
   return (
     <DotLottiePlayer
-      src={
-        "https://stutify.in/lottie/863f36c0-f4d7-4ba3-a87f-262a214ba7d6/HZdnggOmdW.lottie"
-      }
+      src="/lotties/sample.lottie"
       style={{
         height: 350,
         width: 350,
